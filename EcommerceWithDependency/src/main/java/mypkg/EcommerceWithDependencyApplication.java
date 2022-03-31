12f19010b7e28d27c36e77e767b222793ee0c0db
AmalaPhilip <102736513+AmalaPhilip@users.noreply.github.com>
2022-03-31 19:36:57 +0530

    EcommerceWithDependency

---
+++ EcommerceWithDependency/src/main/java/mypkg/EcommerceWithDependencyApplication.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWithDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWithDependencyApplication.class, args);
+	}
+
+}
---
+++ EcommerceWithDependency/src/main/java/mypkg/service/impl/EmployeeService.java
@@ -0,0 +1,12 @@
+package mypkg.service.impl;
+
+import java.util.Set;
+
+import mypkg.model.Employee;
+
+public interface EmployeeService {
+void add(Employee employee);
+	
+	Set<Employee> employee();
+
+}
