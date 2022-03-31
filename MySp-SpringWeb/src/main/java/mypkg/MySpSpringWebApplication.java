a21fc2869652307052e755c9ae574d04594abded
AmalaPhilip <102736513+AmalaPhilip@users.noreply.github.com>
2022-03-31 19:40:23 +0530

    SpringWeb

---
+++ MySp-SpringWeb/src/main/java/mypkg/MySpSpringWebApplication.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpSpringWebApplication.class, args);
+	}
+
+}
